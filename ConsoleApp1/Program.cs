caeeb5f8b3173f3fce904f43ac7e59967060b574
larisa33 <muntoiu.larisa@gmail.com>
2023-10-03 15:21:53 +0300

    tratarea unei impartiri

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,7 +4,59 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int deimpartit = ValidNumber("Introdu deimpartitul: ");
+            int impartitor = ValidNumber("Introdu impartitorul: ");
+
+            while (impartitor == 0)
+            {
+                Console.WriteLine("Impartire la zero! Introdu alt numar.");
+                impartitor = ValidNumber("Introdu impartitorul: ");
+            }
+
+            Impartire(deimpartit, impartitor);
+
+            static void Impartire(int deimpartit, int impartitor)
+            {
+                int rezultat = deimpartit / impartitor;
+                int rest  = deimpartit % impartitor;
+
+                if (rest == 0)
+                {
+                    Console.WriteLine("Rezultatul impartirii: '" + deimpartit + "/" + impartitor + "' este: " + rezultat);
+                } else
+                {
+                    Console.WriteLine("Impartirea poate sa fie efectuata doar cu rest!");
+                    Console.WriteLine("Rezultatul impartirii: '" + deimpartit + "/" + impartitor + "' este: " + rezultat + " rest " + rest);
+                }
+              
+            }
+
+            static int ValidNumber(string eticheta)
+            {
+                bool validNumber = false;
+
+                while (!validNumber)
+                {
+                    Console.Write(eticheta);
+                    string text = Console.ReadLine();
+
+                    validNumber = int.TryParse(text, out int result);
+
+                  
+                    if (validNumber)
+                    {
+                        return result;
+                    }
+                    else 
+                    {
+                        Console.WriteLine("Sorry, the text does not represent a valid age. Please try again!");
+                    }
+
+                }
+
+                return 0;
+
+            }
         }
     }
 }
